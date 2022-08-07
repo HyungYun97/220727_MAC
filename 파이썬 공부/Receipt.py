51567d059190977ef8386eaee6e9da8533f00516
Hyungyun97 <rlaguddbs97@naver.com>
2022-08-07 23:39:18 +0900

    파이썬 공부

---
+++ 파이썬 공부/Receipt.py
@@ -0,0 +1,18 @@
+from calendar import c
+
+
+X = int(input()) # 영수증의 총 금액
+N = int(input()) # 영수증에 적힌 물건의 수
+Z = 0 # 총 금액과 비교할 값
+
+for i in range(X):
+    a,b = map(int,input().split())
+    c = 0
+    c = (a*b)
+    Z += c
+
+if c == X:
+    print("YES")
+else:
+    print("NO")
+
