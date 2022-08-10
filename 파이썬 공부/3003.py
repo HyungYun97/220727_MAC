0c9cb43cbd1f33fa38aa5231f76cf0a71ecc77ba
Hyungyun97 <rlaguddbs97@naver.com>
2022-08-10 22:48:29 +0900

    3003 파이썬

--- 파이썬 공부/3003.py
+++ 파이썬 공부/3003.py
@@ -3,4 +3,4 @@ chess = [1, 1, 2, 2, 2, 8] # 체스판의 개수
 a = list(map(int,input().split())) # a 라는 변수에 리스트로 입력값을 저장한다.
 
 for i in range(6):
-    print(chess[i] - a[i], end = ' ') # 띄어쓰기가 가능하도록 end 함수를 넣어준다. 
+    print(chess[i] - a[i], end = ' ') # 띄어쓰기가 가능하도록 end 함수를 넣어준다 개행문자는 \n 이 디폴트
