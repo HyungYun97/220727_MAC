201c58e2a513bb42f5c14469b435ef778de907ea
Hyungyun97 <rlaguddbs97@naver.com>
2022-08-16 23:56:27 +0900

    2675 다시 공부

--- 파이썬 공부/2675.py
+++ 파이썬 공부/2675.py
@@ -1,11 +1,24 @@
-T = int(input()) # 테스트 케이스의 개수
+
+#T = int(input()) # 테스트 케이스의 개수
+
+#for _ in range(T): #int 문은 항상 range로 묶어야 가능함.
+#    R,S = map(str,input().split()) # R은 반복횟수, S는 문자열
+#    R = int(R)
+
+#    for i in S:
+#        P = list()
+#        P[i] = R*i
+#    print(P)
 
 
-for i in range(T): #int 문은 항상 range로 묶어야 가능함.
-    R,S = map(str,input().split()) # R은 반복횟수, S는 문자열
+#2675_문자열 반복
+T = int(input())
+
+
+for _ in range(T):
+    R, S = list(map(str, input().split()))
     R = int(R)
 
-    for i in range(R):
-        S = R * i
-
-    print(S)
+    for i in S:
+        print(R*i, end='')
+    print()
