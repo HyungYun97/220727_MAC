21ffd7ec7a74fa5fb83dd650a776f9ac629a10af
Hyungyun97 <rlaguddbs97@naver.com>
2022-08-21 23:05:44 +0900

    2098 파이썬

--- 파이썬 공부/2098.py
+++ 파이썬 공부/2098.py
@@ -1,2 +1,14 @@
-A = list(input()) # A 라는 리스트에 저장될 변수들
-B = list(input()) # B 라는 리스트에 저장될 변수들 
+A,B = input().split()
+reverseA = int(A[::-1])
+reverseB = int(B[::-1])
+
+if reverseA > reverseB:
+    print(reverseA)
+
+else:
+    print(reverseB)
+
+
+# 리스트를 사용하지 않은 단순 숫자형 문자들의 역순을 계산해서 풀이한 답안
+
+# 파이썬의 역순을 계산하기 위해서는 [::-1] 을 사용해주면 된다. 
